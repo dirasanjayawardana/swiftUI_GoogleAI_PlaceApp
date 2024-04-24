7566f2483974ab4a250957d54e559c2dcb386561
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-24 10:20:48 +0700

    Initial Commit

---
+++ PlacesGoogleAI/PlacesGoogleAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesGoogleAIApp.swift
+//  PlacesGoogleAI
+//
+//  Created by MACBOOK PRO on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PlacesGoogleAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
